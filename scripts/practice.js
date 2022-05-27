062f0ecfab78350851abf11a782a514294aa716e
kylezparker <kyle.z.parker3@gmail.com>
2022-05-26 22:27:03 -0700

    assignment 2 not finished

--- scripts/practice.js
+++ scripts/practice.js
@@ -21,21 +21,36 @@ console.log(student.address.country,student.address.street);
 
 
 let products=[];
-let product1={
-    name:"laptop",
-    description:"hp laptop 14 in core i7",
-    price:200
+// let product1={
+//     name:"laptop",
+//     description:"hp laptop 14 in core i7",
+//     price:200
+// }
+// let product2={
+//     name:"dvd",
+//     description:"avengers:infinity war",
+//     price:20
+// }
+// let product3={
+//     name:"bluray",
+//     description:"avengers:endgame special edition with deleted scenes and infinity gauntlet",
+//     price:500
+// }
+
+
+//constructor vs object
+// local vars n d p.... this?
+function Product(n,d,p){
+    this.name=n;
+    this.description=d;
+    this.price=p;
 }
-let product2={
-    name:"dvd",
-    description:"avengers:infinity war",
-    price:20
-}
-let product3={
-    name:"bluray",
-    description:"avengers:endgame special edition with deleted scenes and infinity gauntlet",
-    price:500
-}
+
+//new?
+let product1=new Product('speaker', 'sont wireless', 20.50);
+let product2=new Product('laptop', 'hp', 100.00);
+let product3=new Product('laptop', 'mac', 1000.00);
+let product4=new Product('laptop', 'asus', 900.00);
 
 console.log(products);
 console.log(product1,product2,product3);
--- scripts/register.js
+++ scripts/register.js
@@ -12,41 +12,78 @@ let salon={
         close:"5pm"
     },
     phone:"555-555-5555",
-    pets:[
-        {
-            name:"scooby",
-            age:50,
-            gender:"male",
-            breed:"dane",
-            service:"grooming",
-            owner:"shaggy",
-            phone:"8888888888"
+    // pets:[
+    //     {
+    //         name:"scooby",
+    //         age:50,
+    //         gender:"male",
+    //         breed:"dane",
+    //         service:"grooming",
+    //         owner:"shaggy",
+    //         phone:"8888888888"
 
-    },
-    {
-        name:"lemon",
-        age:5,
-        gender:"female",
-        breed:"cat",
-        service:"grooming",
-        owner:"kyle",
-        phone:"8888888888"
+    // },
+    // {
+    //     name:"lemon",
+    //     age:5,
+    //     gender:"female",
+    //     breed:"cat",
+    //     service:"grooming",
+    //     owner:"kyle",
+    //     phone:"8888888888"
 
-    },
-    {
-        name:"amy",
-        age:5,
-        gender:"female",
-        breed:"cat",
-        service:"grooming",
-        owner:"kyle",
-        phone:"8888888888"
+    // },
+    // {
+    //     name:"amy",
+    //     age:5,
+    //     gender:"female",
+    //     breed:"cat",
+    //     service:"grooming",
+    //     owner:"kyle",
+    //     phone:"8888888888"
 
-    }
-    ]
+    // }
+    // ]
+    pets:[]
 }
 
-console.log(salon.pets[0],salon.pets[1]);
+
+function Pet(name,age,gender,breed,service,owner,phone){
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    this.breed=breed;
+    this.service=service;
+    this.owner=owner; //
+    this.phone=phone; //
+}
+
+let amy=new Pet('amy', 5, "female","cat","haircut","kyle",55555555555);
+let lemon=new Pet('lemon', 5, "female","cat","haircut","kyle",55555555555);
+let george=new Pet('george', 5, "male","cat","haircut","kyle",55555555555);
+
+function register(){
+console.log("Registering");
+//get the info from the inputs ****getElementById  .value
+let petName=document.getElementById("txtName").value;
+let petAge=document.getElementById("txtAge").value;
+let petGender=document.getElementById("txtGender").value;
+let petBreed=document.getElementById("txtBreed").value;
+let petService=document.getElementById("selService").value;
+
+//create the object using the constructor
+let newPet= new Pet(petName,petAge,petGender,petBreed,petService);
+//push the object in on the array
+salon.pets.push(newPet);
+//display it on the console
+console.log(newPet);
+console.log(salon.pets);
+}
+
+
+
+//console.log(salon.pets[0],salon.pets[1]);
+
 //call function to certain location in html
 //lookup document.getElementById.innerHTML
 console.log(salon);
@@ -54,15 +91,28 @@ function displaySalonInfo(){
     //document.getElementById("salon").innerHTML=salon;
     let tmp=`<p>welcome to the ${salon.name}</p><p>located at ${salon.address.street} ${salon.address.zip} ${salon.address.city} ${salon.address.number}</p><p>hours ${salon.hours.open}-${salon.hours.close}</p><p>phone number ${salon.phone}</p>`;
     document.getElementById("info").innerHTML=tmp;
+    console.log("displaying");
 
 }
+
+
+
+
+// function displayNames(){
+//     for(let i=0;i<salon.pets.length;i++){
+//         console.log(salon.pets[i].name);
+//     }
+// }
+// displayNames();
+
+// alert(`${salon.pets.length}`);
+
+
+function init(){
+//hook events
 displaySalonInfo();
-
-function displayNames(){
-    for(let i=0;i<salon.pets.length;i++){
-        console.log(salon.pets[i].name);
-    }
+console.log(amy,lemon);
+salon.pets.push(amy,lemon);
 }
-displayNames();
 
-
+window.onload=init;
